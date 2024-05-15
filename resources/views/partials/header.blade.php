895e3a14ea281d890239cff51703d73854083ea4
MarioBroi <inoix16@gmail.com>
2024-05-15 16:22:19 +0100

    add header tags with routes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,9 @@
 <h1>site header</h1>
+<div class="container d-flex">
+    <div>
+        <a href="{{ route('comics.index') }}">Comics</a>
+    </div>
+    <div class="px-4">
+        <a href="{{ route('comics.create') }}">Create</a>
+    </div>
+</div>
