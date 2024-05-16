e59d37835c3f00e55229dc32cd194bc4bb9d3c4a
MarioBroi <inoix16@gmail.com>
2024-05-16 15:08:55 +0100

    add scss partials, edit header layout and style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,71 @@
-<h1>site header</h1>
-<div class="container d-flex">
-    <div>
-        <a href="{{ route('comics.index') }}">Comics</a>
+<header class="site-header bg-primary">
+
+    <div class="container">
+
+        <div class="top d-flex justify-content-end text-light">
+            <small>DC POWER™ VISA®</small>
+            <small>ADDITIONAL DC SITES</small>
+        </div>
+        <!-- .top -->
+
     </div>
-    <div class="px-4">
-        <a href="{{ route('comics.create') }}">Create</a>
+    <!-- .container -->
+
+    <div class="bg-white">
+
+        <nav class="navbar navbar-expand-sm navbar-light d-flex">
+
+            <div class="container">
+
+                <a class="navbar-brand" href="#">
+                    <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="">
+                </a>
+
+                <ul class="list-unstyled d-flex">
+                    <li class="px-2">
+                        <a href="{{ route('comics.index') }}">COMICS</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="{{ route('comics.create') }}">CREATE</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="">MOVIES</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="">TV</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="">GAMES</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="">COLLECTIBLES</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="">VIDEOS</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="">FANS</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="">NEWS</a>
+                    </li>
+                    <li class="px-2">
+                        <a href="">SHOP</a>
+                    </li>
+                </ul>
+
+                <span>
+                    <input type="text" name="search" id="search" placeholder="Search">
+                </span>
+
+            </div>
+            <!-- .container -->
+
+        </nav>
+        <!-- .navbar -->
+
     </div>
-</div>
+    <!-- /.bg-white -->
+
+</header>
+<!-- /.site-header -->
