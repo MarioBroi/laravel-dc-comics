a99765ae709729a782db0fb7ea842eb60f3ab258
MarioBroi <inoix16@gmail.com>
2024-05-16 15:56:07 +0100

    add layout in welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,46 @@
     </jumbotron>
     <!-- /.jumbotron -->
 
+    <section class="welcome-text">
+        <div class="container">
+
+            <h1>Welcome to DC Universe Comics Shop!</h1>
+
+            <p>Dive into the vibrant and heroic world of DC Comics with us! Our store is your ultimate destination for all
+                things DC, from classic tales to the latest adventures of your favorite superheroes. Whether you're a
+                lifelong
+                fan or a newcomer to the universe, we have something for everyone.
+            </p>
+
+            <p>
+                Explore our extensive collection featuring iconic characters like Batman, Superman, Wonder Woman, and The
+                Flash.
+                Discover epic storylines, rare editions, and exclusive merchandise that will elevate your comic collection
+                to
+                new heights.
+            </p>
+
+            <p>
+                At DC Universe Comics Shop, we pride ourselves on offering not just comics, but a gateway to extraordinary
+                worlds
+                where heroes rise, villains scheme, and epic battles unfold. Our passion for comics drives us to provide
+                top-notch
+                customer service and a curated selection that caters to both seasoned collectors and enthusiastic readers.
+            </p>
+
+            <p>
+                Join our community of comic enthusiasts and embark on adventures that leap off the pages. Stay updated with
+                new
+                releases, special promotions, and events that celebrate the rich legacy of DC Comics.
+                Unleash your inner hero. Welcome to the DC Universe Comics Shop – Where Legends Live On.
+            </p>
+
+        </div>
+        <!-- /.container -->
+
+    </section>
+    <!-- /.welcome-text -->
+
     <section class="shop">
         <div class="container">
             <ul class="list-unstyled d-flex justify-content-center">
