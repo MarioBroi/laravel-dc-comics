5d297ee725e044232903bde2668d453f24dcdb1f
MarioBroi <inoix16@gmail.com>
2024-05-16 15:10:57 +0100

    add img folder with images, edit site footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,80 @@
-<h1>site footer</h1>
+<footer class="site-footer">
+    <div class="container">
+
+        <section class="footer-top">
+
+            <div class="d-flex">
+
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Voluptatum.</a></li>
+                    <li><a href="">Ipsam?</a></li>
+                    <li><a href="">Mollitia.</a></li>
+                    <li><a href="">Provident?</a></li>
+                    <li><a href="">Natus?</a></li>
+                    <li><a href="">Animi.</a></li>
+                </ul>
+
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Cupiditate.</a></li>
+                    <li><a href="">Aperiam.</a></li>
+                    <li><a href="">Aperiam.</a></li>
+                    <li><a href="">Nihil?</a></li>
+                    <li><a href="">Dolorem?</a></li>
+                    <li><a href="">Quam.</a></li>
+                </ul>
+
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Voluptatum.</a></li>
+                    <li><a href="">Ipsam?</a></li>
+                    <li><a href="">Mollitia.</a></li>
+                    <li><a href="">Provident?</a></li>
+                    <li><a href="">Natus?</a></li>
+                    <li><a href="">Animi.</a></li>
+                </ul>
+
+                <ul class="list-unstyled">
+                    <li><a href="">Lorem.</a></li>
+                    <li><a href="">Cupiditate.</a></li>
+                    <li><a href="">Aperiam.</a></li>
+                    <li><a href="">Aperiam.</a></li>
+                    <li><a href="">Nihil?</a></li>
+                    <li><a href="">Dolorem?</a></li>
+                    <li><a href="">Quam.</a></li>
+                </ul>
+
+            </div>
+
+            <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint architecto eum minus, quo cupiditate
+                similique vero ipsam! Est quas temporibus nisi corrupti minima libero facilis asperiores rem natus?
+                Doloribus, culpa.
+            </p>
+
+        </section>
+        <!-- /.footer-top -->
+
+        <section class="footer-bot">
+            <div class="d-flex justify-content-between">
+                <button>
+                    SIGN-UP NOW
+                </button>
+                <div>
+                    <a href="">FOLLOW US</a>
+                    <a href="">logo</a>
+                    <a href="">logo</a>
+                    <a href="">logo</a>
+                    <a href="">logo</a>
+                    <a href="">logo</a>
+                </div>
+            </div>
+        </section>
+        <!-- /.footer-bot -->
+
+    </div>
+    <!-- /.container -->
+
+</footer>
+<!-- /.site-footer -->
