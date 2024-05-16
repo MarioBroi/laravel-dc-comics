63ce0f9e2f09bc4bad6fd44205707c852ff8d09f
MarioBroi <inoix16@gmail.com>
2024-05-16 13:45:28 +0100

    add anchor tag with rout to show the single item

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -4,19 +4,21 @@
         <div class="row row-cols-3 row-cols-lg-2 py-3 g-4">
             @forelse ($comics as $comic)
                 <div class="col">
-                    <div class="card">
-                        <img class="card-img-top" src="{{ $comic->thumb }}" alt="Title" />
-                        <div class="card-body">
-                            <h4 class="card-title">Title: {{ $comic->title }}</h4>
-                            <p class="card-text">{{ $comic->id }}</p>
-                            <p class="card-text">{{ $comic->description }}</p>
-                            <p class="card-text">Price: {{ $comic->price }}</p>
-                            <p class="card-text">Series: {{ $comic->series }}</p>
-                            <p class="card-text">Sale date: {{ $comic->sale_date }}</p>
-                            <p class="card-text">Type: {{ $comic->type }}</p>
+                    <a href="{{ route('comics.show', $comic) }}">
+                        <div class="card">
+                            <img class="card-img-top" src="{{ $comic->thumb }}" alt="Title" />
+                            <div class="card-body">
+                                <h4 class="card-title">Title: {{ $comic->title }}</h4>
+                                <p class="card-text">{{ $comic->id }}</p>
+                                <p class="card-text">{{ $comic->description }}</p>
+                                <p class="card-text">Price: {{ $comic->price }}</p>
+                                <p class="card-text">Series: {{ $comic->series }}</p>
+                                <p class="card-text">Sale date: {{ $comic->sale_date }}</p>
+                                <p class="card-text">Type: {{ $comic->type }}</p>
+                            </div>
                         </div>
-                    </div>
-                    <!-- /.card -->
+                        <!-- /.card -->
+                    </a>
                 </div>
                 <!-- /.col -->
             @empty
