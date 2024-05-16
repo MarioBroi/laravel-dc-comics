d802d774a5cb31317324bce310320c9d5076d521
MarioBroi <inoix16@gmail.com>
2024-05-16 13:59:20 +0100

    edit show page layout for the single comic show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -5,9 +5,16 @@
         <div class="row">
             <div class="col">
                 <div class="card">
-                    <p>{{ $comic->id }}</p>
-                    <p>{{ $comic->title }}</p>
-                    <p>{{ $comic->description }}</p>
+                    <img class="card-img-top" src="{{ $comic->thumb }}" alt="Title" />
+                    <div class="card-body">
+                        <h4 class="card-title">Title: {{ $comic->title }}</h4>
+                        <p class="card-text">{{ $comic->id }}</p>
+                        <p class="card-text">{{ $comic->description }}</p>
+                        <p class="card-text">Price: {{ $comic->price }}</p>
+                        <p class="card-text">Series: {{ $comic->series }}</p>
+                        <p class="card-text">Sale date: {{ $comic->sale_date }}</p>
+                        <p class="card-text">Type: {{ $comic->type }}</p>
+                    </div>
                 </div>
                 <!-- /.card -->
             </div>
