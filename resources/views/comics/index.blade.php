26195104b913b676d7ca6d9f2d70de077c7f4a71
MarioBroi <inoix16@gmail.com>
2024-05-16 13:34:42 +0100

    edit index layout

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,14 +1,19 @@
 @extends('layout.app')
 @section('content')
     <div class="container">
-        <div class="row">
+        <div class="row row-cols-3 row-cols-lg-4 py-3 g-4">
             @forelse ($comics as $comic)
                 <div class="col">
                     <div class="card">
-                        <p>{{ $comic->id }}</p>
-                        <p>{{ $comic->title }}</p>
-                        <p>{{ $comic->description }}</p>
-                        <p><a href="{{ route('comics.show', $comic) }}">View</a></p>
+                        <img class="card-img-top" src="{{ $comic->thumb }}" alt="Title" />
+                        <div class="card-body">
+                            <h4 class="card-title">Title: {{ $comic->title }}</h4>
+                            <p class="card-text">{{ $comic->description }}</p>
+                            <p class="card-text">Price: {{ $comic->price }}</p>
+                            <p class="card-text">Series: {{ $comic->series }}</p>
+                            <p class="card-text">Sale date: {{ $comic->sale_date }}</p>
+                            <p class="card-text">Type: {{ $comic->type }}</p>
+                        </div>
                     </div>
                     <!-- /.card -->
                 </div>
